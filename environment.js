3cb1ffc0480b6f2f3264e551c78a240526ecea2e
TechExplorerAnish <anishghimire178@gmail.com>
2025-12-23 08:48:59 +0545

    draw environment in canvas

--- environment.js
+++ environment.js
@@ -9,4 +9,28 @@ class Environment{
         this.start = start;
         this.goal = goal;
     }
+
+    draw(ctx, cellSize, offsetX = 0, offsetY = 0) {
+        // Draw the grid
+        ctx.strokeStyle = 'white';
+        for (let i = 0; i <= this.gridSize; i++) {
+            ctx.beginPath();
+            ctx.moveTo(offsetX + i * cellSize, offsetY);
+            ctx.lineTo(offsetX + i * cellSize, offsetY + this.gridSize * cellSize);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(offsetX, offsetY + i * cellSize);
+            ctx.lineTo(offsetX + this.gridSize * cellSize, offsetY + i * cellSize);
+            ctx.stroke();
+        }
+
+        // Draw the start position
+        ctx.fillStyle = 'lightblue';
+        ctx.fillRect(offsetX + this.start.x * cellSize, offsetY + this.start.y * cellSize, cellSize, cellSize);
+
+        // Draw the goal position
+        ctx.fillStyle = 'lightgreen';
+        ctx.fillRect(offsetX + this.goal.x * cellSize, offsetY + this.goal.y * cellSize, cellSize, cellSize);
+
+    }
 }
--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 //Problem Move an agent on grid to reach a goal position
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+const width = 800;
+const height = 800;
+
+const cellSize = 50; // Size of each grid cell
+const gridSize = 5; // 5x5 grid
+const offsetX = (cellSize * gridSize); // Offset for drawing the grid
+const offsetY = (cellSize * gridSize); // Offset for drawing the grid
+
+const start = { x: 0, y: 0 }; // Starting position of the agent
+const goal = { x: 4, y: 4 }; // Goal position
+
+
+canvas.width = width;
+canvas.height = height;
+
+const environment = new Environment(gridSize, start, goal);
+
+environment.draw(ctx, cellSize, offsetX, offsetY);
